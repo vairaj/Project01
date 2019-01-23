4a95c595f4fc62c8215da8b9a81e3f954228359c
vairaj <43154018+vairaj@users.noreply.github.com>
2019-01-23 02:59:04 -0700

    Add files via upload

--- Proj1.cpp
+++ Proj1.cpp
@@ -14,7 +14,7 @@ void BloodSugar::input(double x, double sugarLevels[14][100])
 
 void BloodSugar::day(double sugarLevels[14][100])
 {
-	cout << "Blood sugar taken " << dailyCount << " times today.\n"
+	cout << "Blood sugar taken " << dailyCount << " time(s) today.\n"
 		<< "Highest: " << dailyMax << endl
 		<< "Lowest: " << dailyMin << endl
 		<< "Total: " << dailySum << endl;
@@ -22,7 +22,7 @@ void BloodSugar::day(double sugarLevels[14][100])
 
 void BloodSugar::week(double sugarLeels[14][100])
 {
-	cout << "Blood sugar taken " << weeklyCount << " times this week.\n"
+	cout << "Blood sugar taken " << weeklyCount << " time(s) this week.\n"
 		<< "Highest: " << weeklyMax << endl
 		<< "Lowest: " << weeklyMin << endl
 		<< "Total: " << weeklySum << endl
@@ -34,8 +34,17 @@ void BloodSugar::next(int x, double sugarLevels[14][100])
 	cout << "Day " << x + 1 << endl;
 	sugarLevels[x][0];
 	dailyCount = 0; //reset daily count
-	if (x == 7)
+	dailyMax = 0; //ditto for rest of day vars
+	dailyMin = 0;
+	dailySum = 0;
+
+	if (x == 7) //when first day of next week ocurrs
 	{
 		weeklyCount = 0; //reset weekly count
+		weeklyMax = 0; //ditto for rest of week vars
+		weeklyMin = 0;
+		weeklySum = 0;
+		delta = 0;
+		cout << "(New Week)\n";
 	}
 }
